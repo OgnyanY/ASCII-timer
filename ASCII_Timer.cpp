2d58ede9123f552d3d34342885bc40cb4f708b9d
OgnyanY <o.yonchev@abv.bg>
2021-01-04 20:18:23 +0200

    changing the template

--- ASCII_Timer.cpp
+++ ASCII_Timer.cpp
@@ -1,194 +1,177 @@
 #include <iostream>
 #include <iomanip>
-#include <windows.h>
 #include <unistd.h>
-
+#include <string>
 using namespace std;
 
-void Zero (int number) {
-    cout << "          " << endl;
-    cout << "0000000000" << endl;
-    cout << "0        0" << endl;
-    cout << "0        0" << endl;
-    cout << "0        0" << endl;
-    cout << "0        0" << endl;
-    cout << "0        0" << endl;
-    cout << "0        0" << endl;
-    cout << "0        0" << endl;
-    cout << "0        0" << endl;
-    cout << "0        0" << endl;
-    cout << "0000000000" << endl;
-    cout << "          " << endl;
+
+void ClearScreen() {
+    cout << string( 100, '\n' );
 }
-void One (int number) {
-    cout << "          " << endl;
-    cout << "         1" << endl;
-    cout << "         1" << endl;
-    cout << "         1" << endl;
-    cout << "         1" << endl;
-    cout << "         1" << endl;
-    cout << "         1" << endl;
-    cout << "         1" << endl;
-    cout << "         1" << endl;
-    cout << "         1" << endl;
-    cout << "         1" << endl;
-    cout << "         1" << endl;
-    cout << "          " << endl;
-}
-void Two (int number) {
-    cout << "          " << endl;
-    cout << "2222222222" << endl;
-    cout << "         2" << endl;
-    cout << "         2" << endl;
-    cout << "         2" << endl;
-    cout << "         2" << endl;
-    cout << "2222222222" << endl;
-    cout << "2         " << endl;
-    cout << "2         " << endl;
-    cout << "2         " << endl;
-    cout << "2         " << endl;
-    cout << "2222222222" << endl;
-    cout << "          " << endl;
-}
-void Tree (int number) {
-    cout << "          " << endl;
-    cout << "3333333333" << endl;
-    cout << "         3" << endl;
-    cout << "         3" << endl;
-    cout << "         3" << endl;
-    cout << "         3" << endl;
-    cout << "3333333333" << endl;
-    cout << "         3" << endl;
-    cout << "         3" << endl;
-    cout << "         3" << endl;
-    cout << "         3" << endl;
-    cout << "3333333333" << endl;
-    cout << "          " << endl;
-}
-void Four (int number) {
-    cout << "          " << endl;
-    cout << "4        4" << endl;
-    cout << "4        4" << endl;
-    cout << "4        4" << endl;
-    cout << "4        4" << endl;
-    cout << "4        4" << endl;
-    cout << "4444444444" << endl;
-    cout << "         4" << endl;
-    cout << "         4" << endl;
-    cout << "         4" << endl;
-    cout << "         4" << endl;
-    cout << "         4" << endl;
-    cout << "          " << endl;
-}
-void Five (int number) {
-    cout << "          " << endl;
-    cout << "5555555555" << endl;
-    cout << "5         " << endl;
-    cout << "5         " << endl;
-    cout << "5         " << endl;
-    cout << "5         " << endl;
-    cout << "5555555555" << endl;
-    cout << "         5" << endl;
-    cout << "         5" << endl;
-    cout << "         5" << endl;
-    cout << "         5" << endl;
-    cout << "5555555555" << endl;
-    cout << "          " << endl;
-}
-void Six (int number) {
-    cout << "          " << endl;
-    cout << "6666666666" << endl;
-    cout << "6         " << endl;
-    cout << "6         " << endl;
-    cout << "6         " << endl;
-    cout << "6         " << endl;
-    cout << "6666666666" << endl;
-    cout << "6        6" << endl;
-    cout << "6        6" << endl;
-    cout << "6        6" << endl;
-    cout << "6        6" << endl;
-    cout << "6666666666" << endl;
-    cout << "          " << endl;
-}
-void Seven (int number) {
-    cout << "          " << endl;
-    cout << "7777777777" << endl;
-    cout << "         7" << endl;
-    cout << "         7" << endl;
-    cout << "         7" << endl;
-    cout << "         7" << endl;
-    cout << "         7" << endl;
-    cout << "         7" << endl;
-    cout << "         7" << endl;
-    cout << "         7" << endl;
-    cout << "         7" << endl;
-    cout << "         7" << endl;
-    cout << "          " << endl;
-}
-void Eight (int number) {
-    cout << "          " << endl;
-    cout << "8888888888" << endl;
-    cout << "8        8" << endl;
-    cout << "8        8" << endl;
-    cout << "8        8" << endl;
-    cout << "8        8" << endl;
-    cout << "8888888888" << endl;
-    cout << "8        8" << endl;
-    cout << "8        8" << endl;
-    cout << "8        8" << endl;
-    cout << "8        8" << endl;
-    cout << "8888888888" << endl;
-    cout << "          " << endl;
-}
-void Nine (int number) {
-    cout << "          " << endl;
-    cout << "          " << endl;
-    cout << "9999999999" << endl;
-    cout << "9        9" << endl;
-    cout << "9        9" << endl;
-    cout << "9        9" << endl;
-    cout << "9        9" << endl;
-    cout << "9999999999" << endl;
-    cout << "         9" << endl;
-    cout << "         9" << endl;
-    cout << "         9" << endl;
-    cout << "         9" << endl;
-    cout << "9999999999" << endl;
-    cout << "          " << endl;
+
+void Template (int number) {
+    if (number == 0) {
+        cout << "0000000000" << endl;
+        cout << "0        0" << endl;
+        cout << "0        0" << endl;
+        cout << "0        0" << endl;
+        cout << "0        0" << endl;
+        cout << "0        0" << endl;
+        cout << "0        0" << endl;
+        cout << "0        0" << endl;
+        cout << "0        0" << endl;
+        cout << "0        0" << endl;
+        cout << "0000000000" << endl;
+        cout << "\n\n\n\n\n\n" << endl;
+    }
+    if (number == 1) {
+        cout << "         1" << endl;
+        cout << "         1" << endl;
+        cout << "         1" << endl;
+        cout << "         1" << endl;
+        cout << "         1" << endl;
+        cout << "         1" << endl;
+        cout << "         1" << endl;
+        cout << "         1" << endl;
+        cout << "         1" << endl;
+        cout << "         1" << endl;
+        cout << "         1" << endl;
+        cout << "\n\n\n\n\n\n" << endl;
+    }
+    if (number == 2) {
+        cout << "2222222222" << endl;
+        cout << "         2" << endl;
+        cout << "         2" << endl;
+        cout << "         2" << endl;
+        cout << "         2" << endl;
+        cout << "2222222222" << endl;
+        cout << "2         " << endl;
+        cout << "2         " << endl;
+        cout << "2         " << endl;
+        cout << "2         " << endl;
+        cout << "2222222222" << endl;
+        cout << "\n\n\n\n\n\n" << endl;
+    }
+    if (number == 3) {
+        cout << "3333333333" << endl;
+        cout << "         3" << endl;
+        cout << "         3" << endl;
+        cout << "         3" << endl;
+        cout << "         3" << endl;
+        cout << "3333333333" << endl;
+        cout << "         3" << endl;
+        cout << "         3" << endl;
+        cout << "         3" << endl;
+        cout << "         3" << endl;
+        cout << "3333333333" << endl;
+        cout << "\n\n\n\n\n\n" << endl;
+    }
+    if (number == 4) {
+        cout << "4        4" << endl;
+        cout << "4        4" << endl;
+        cout << "4        4" << endl;
+        cout << "4        4" << endl;
+        cout << "4        4" << endl;
+        cout << "4444444444" << endl;
+        cout << "         4" << endl;
+        cout << "         4" << endl;
+        cout << "         4" << endl;
+        cout << "         4" << endl;
+        cout << "         4" << endl;
+        cout << "\n\n\n\n\n\n" << endl;
+    }
+    if (number == 5) {
+        cout << "5555555555" << endl;
+        cout << "5         " << endl;
+        cout << "5         " << endl;
+        cout << "5         " << endl;
+        cout << "5         " << endl;
+        cout << "5555555555" << endl;
+        cout << "         5" << endl;
+        cout << "         5" << endl;
+        cout << "         5" << endl;
+        cout << "         5" << endl;
+        cout << "5555555555" << endl;
+        cout << "\n\n\n\n\n\n" << endl;
+    }
+    if (number == 6) {
+        cout << "6666666666" << endl;
+        cout << "6         " << endl;
+        cout << "6         " << endl;
+        cout << "6         " << endl;
+        cout << "6         " << endl;
+        cout << "6666666666" << endl;
+        cout << "6        6" << endl;
+        cout << "6        6" << endl;
+        cout << "6        6" << endl;
+        cout << "6        6" << endl;
+        cout << "6666666666" << endl;
+        cout << "\n\n\n\n\n\n" << endl;
+    }
+    if (number == 7) {
+        cout << "7777777777" << endl;
+        cout << "         7" << endl;
+        cout << "         7" << endl;
+        cout << "         7" << endl;
+        cout << "         7" << endl;
+        cout << "         7" << endl;
+        cout << "         7" << endl;
+        cout << "         7" << endl;
+        cout << "         7" << endl;
+        cout << "         7" << endl;
+        cout << "         7" << endl;
+        cout << "\n\n\n\n\n\n" << endl;
+    }
+    if (number == 8) {
+        cout << "8888888888" << endl;
+        cout << "8        8" << endl;
+        cout << "8        8" << endl;
+        cout << "8        8" << endl;
+        cout << "8        8" << endl;
+        cout << "8888888888" << endl;
+        cout << "8        8" << endl;
+        cout << "8        8" << endl;
+        cout << "8        8" << endl;
+        cout << "8        8" << endl;
+        cout << "8888888888" << endl;
+        cout << "\n\n\n\n\n\n" << endl;
+    }
+    if (number == 9) {
+        cout << "9999999999" << endl;
+        cout << "9        9" << endl;
+        cout << "9        9" << endl;
+        cout << "9        9" << endl;
+        cout << "9        9" << endl;
+        cout << "9999999999" << endl;
+        cout << "         9" << endl;
+        cout << "         9" << endl;
+        cout << "         9" << endl;
+        cout << "         9" << endl;
+        cout << "9999999999" << endl;
+        cout << "\n\n\n\n\n\n" << endl;
+    }
 }
 
 int main()
 {
-	HWND console = GetConsoleWindow();
-	RECT ConsoleRect;
-	GetWindowRect(console, &ConsoleRect);
-
-   MoveWindow(console, ConsoleRect.left, ConsoleRect.top, 800, 250, TRUE);
-
-   system ("pause");
-
 
     int number = 0;
-    Zero (number);
+    cin >> number;
+    ClearScreen();
+    Template (number);
     sleep(1);//sleeps for 1 second
-    One (number);
+    ClearScreen();
+    Template (number);
     sleep(1);//sleeps for 1 second
-    Two (number);
+    ClearScreen();
+    Template (number);
     sleep(1);//sleeps for 1 second
-    Tree (number);
+    ClearScreen();
+    Template (number);
     sleep(1);//sleeps for 1 second
-    Four (number);
-    sleep(1);//sleeps for 1 second
-    Five (number);
-    sleep(1);//sleeps for 1 second
-    Six (number);
-    sleep(1);//sleeps for 1 second
-    Seven (number);
-    sleep(1);//sleeps for 1 second
-    Eight (number);
-    sleep(1);//sleeps for 1 second
-    Nine (number);
-    sleep(1);//sleeps for 1 second
+    ClearScreen();
     cout << '\a';
+    Template (number);
+
     return 0;
 }
